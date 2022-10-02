b6771dbebe519a9341dea9059ee7a9c3eea7fcff
joshuavd <joshuavd@att.net>
2022-10-02 11:32:35 -0700

    added logging

--- lab2/src/main/java/com/SalesMapper.java
+++ lab2/src/main/java/com/SalesMapper.java
@@ -4,17 +4,21 @@ import java.io.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.Mapper.*;
+import org.apache.log4j.Logger;
 
 public class SalesMapper extends
          Mapper<LongWritable, Text, Text, IntWritable> {
+   private static final Logger THE_LOGGER =
+      Logger.getLogger(SalesMapper.class);
+
    @Override
    public void map(LongWritable key, Text value, Context context)
          throws IOException, InterruptedException {
       String valueAsString = value.toString().trim();
       String[] tokens = valueAsString.split(" ");
-      if (tokens.length != 5) {
-         return;
-      }
+      
+      THE_LOGGER.info(tokens, tokens.length);
+
       context.write(new Text(tokens[1]),
                   new IntWritable(1));
    }
--- lab2/src/main/java/com/SalesReducer.java
+++ lab2/src/main/java/com/SalesReducer.java
@@ -7,6 +7,9 @@ import org.apache.hadoop.mapreduce.Reducer.*;
 
 public class SalesReducer
 extends Reducer<Text, IntWritable, Text, IntWritable> {
+   private static final Logger THE_LOGGER =
+      Logger.getLogger(SalesReducer.class);
+
    @Override
    public void reduce(Text date,
    Iterable<IntWritable> sales, Context context)
