fd78d4e268ddb32ab66e2f70c6b395b04ff8fd56
joshuavd <joshua.vondamm@packetcraft.com>
2022-10-06 11:23:14 -0700

    added imports to partitioner class

--- lab3/src/main/java/com/SecondarySortPartitioner.java
+++ lab3/src/main/java/com/SecondarySortPartitioner.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import org.apache.hadoop.io.*;
 import org.apache.log4j.Logger;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.Partitioner;
 
 public class SecondarySortPartitioner
                           // < key, value >
