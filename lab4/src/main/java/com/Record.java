100300ae8fd6876c10fe7bb911e8806707c6a210
joshuavd <joshua.vondamm@packetcraft.com>
2022-10-11 14:39:27 -0700

    added back toString method for java

--- lab4/src/main/java/com/Record.java
+++ lab4/src/main/java/com/Record.java
@@ -2,15 +2,15 @@ public class Record implements Comparable <Record>{
     private int id;
     private String categoryName;
     private double weight;
-    
+
     public Record(int id, String categoryName, double weight) {
         this.id = id;
         this.categoryName = categoryName;
         this.weight = weight;
     }
-    // public String toString() {
-    //     return id + "," + categoryName + "," + weight;
-    // }
+    public String toString() {
+        return id + "," + categoryName + "," + weight;
+    }
     public int compareTo(Record other) {
         if(this.weight > other.weight) return -1;
         if(this.weight < other.weight) return 1;
