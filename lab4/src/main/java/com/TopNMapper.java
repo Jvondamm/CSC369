e583c26cfa96603e3e9edc06476bb5cb865b84ef
joshuavd <joshua.vondamm@packetcraft.com>
2022-10-11 14:15:11 -0700

    debug statements added

--- lab4/src/main/java/com/TopNMapper.java
+++ lab4/src/main/java/com/TopNMapper.java
@@ -20,6 +20,7 @@ public class TopNMapper extends
         String line = value.toString().trim();
         String[] tokens = line.split(",");
         THE_LOGGER.info(Arrays.toString(tokens));
+        THE_LOGGER.info(tokens[2]);
         double weight = Double.parseDouble(tokens[2].trim());
         top.add(new Record(Integer.parseInt(tokens[0].trim()),tokens[1].trim(),weight));
 
