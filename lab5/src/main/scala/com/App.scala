d84978dff4f65f8f28e773a1429715aac9bc343c
joshuavd <joshua.vondamm@packetcraft.com>
2022-10-18 11:55:27 -0700

    added comments and removed import

--- lab5/src/main/scala/com/App.scala
+++ lab5/src/main/scala/com/App.scala
@@ -1,7 +1,6 @@
 package example
 import scala.io._
 import scala.collection._
-import scala.math.BigDecimal
 
 object App {
   def main(args: Array[String]): Unit = {
@@ -11,21 +10,26 @@ object App {
     store.map(x =>(x._6, x._1, saleTotal(x._1))).sortBy(_._1).foreach(println(_))
   }
 
+  /* id, name, address, city, zip, state, phone */
   def store: List[(String, String, String, String, String, String, String)] = {
     return Source.fromFile("store.csv").getLines().toList.map(x =>
         (x.split(",")(0), x.split(",")(1), x.split(",")(2), x.split(",")(3), x.split(",")(4),x.split(",")(5),x.split(",")(6)))
   }
 
+  /* id, date, time, storeid, customerid */
   def sales: List[(String, String, String, String, String)] = {
     return Source.fromFile("sales.csv").getLines().toList.map(x =>
         (x.split(",")(0), x.split(",")(1), x.split(",")(2), x.split(",")(3), x.split(",")(4)))
   }
 
+  /* map so we can call it directly with lineItem product */
+  /* id, item, price */
   def product: Map[String, Double] = {
     return Source.fromFile("product.csv").getLines().toList.map(x =>
         x.split(",")(0) -> "%.2f".format(x.split(",")(2).toDouble).toDouble).toMap
   }
 
+  /* id, saleid, productid, number */
   def lineItem: List[(String, String, String, Int)] = {
     return Source.fromFile("lineItem.csv").getLines().toList.map(x =>
         (x.split(",")(0), x.split(",")(1), x.split(",")(2), x.split(",")(3).toInt))
