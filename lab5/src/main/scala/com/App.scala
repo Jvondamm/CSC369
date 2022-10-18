daac298adb78cc730683aa1d8f4a78d5857a5fef
joshuavd <joshuavd@att.net>
2022-10-17 19:49:16 -0700

    fixed some more complation errors

--- lab5/src/main/scala/com/App.scala
+++ lab5/src/main/scala/com/App.scala
@@ -5,7 +5,7 @@ import scala.collection._
 object App {
   def main(args: Array[String]): Unit = {
     val products = product
-    val saleTotal = lineItem.map(x => (x._2, products(x._4) * x._4)).groupBy(_._1).mapValues(_.map(_._2).sum)
+    val saleTotal = lineItem.map(x => (x._2, products(x._3) * x._4)).groupBy(_._1).mapValues(_.map(_._2).sum)
     val storeTotal = sales.map(x => (x._4, saleTotal(x._1))).groupBy(_._1).mapValues(_.map(_._2).sum)
     store.map(x =>(x._6, x._1, saleTotal(x._1))).sortBy(_._1).foreach(println(_))
   }
@@ -20,9 +20,9 @@ object App {
         (x.split(", ")(0), x.split(", ")(1), x.split(", ")(2), x.split(", ")(3), x.split(", ")(4)))
   }
 
-  def product: List[(String, String, Double)] = {
+  def product: Map[String, Double] = {
     return Source.fromFile("product.csv").getLines().toList.map(x =>
-        (x.split(", ")(0), x.split(", ")(1), x.split(", ")(2).toDouble))
+        x.split(", ")(0) -> x.split(", ")(2).toDouble).toMap
   }
 
   def lineItem: List[(String, String, String, Int)] = {
