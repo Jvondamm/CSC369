7a759e23ed10e5e9631bd018399d50b30e201eec
joshuavd <joshuavd@att.net>
2022-10-26 12:08:29 -0700

    call me russia with how hard im getting fucked

--- bonus/src/main/java/com/Driver.java
+++ bonus/src/main/java/com/Driver.java
@@ -7,26 +7,24 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 
-public class SalesDriver extends Configured
+public class Driver extends Configured
                                          implements Tool {
     private static final Logger THE_LOGGER =
-             Logger.getLogger(SalesDriver.class);
+             Logger.getLogger(Driver.class);
 
     @Override
     public int run(String[] args) throws Exception {
         Job job = Job.getInstance();
-        job.setJarByClass(SalesDriver.class);
-        job.setJobName("SalesDriver");
+        job.setJarByClass(Driver.class);
+        job.setJobName("Driver");
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        job.setMapOutputKeyClass(Sales.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setMapperClass(SecondarySortMapper.class);
-        job.setCombinerClass(SecondarySortReducer.class);
-        job.setReducerClass(SecondarySortReducer.class);
-        job.setPartitionerClass(SecondarySortPartitioner.class);
-        job.setGroupingComparatorClass(SecondarySortGroupingComparator.class);
-        job.setSortComparatorClass(SecondarySortSortingComparator.class);
+        job.setMapOutputKeyClass(PairOfStrings.class);
+        job.setMapOutputValueClass(PairOfStrings.class);
+        job.setMapperClass(LineItemMapper1.class);
+        job.setReducerClass(Reducer1.class);
+        job.setPartitionerClass(Partitioner1.class);
+        job.setGroupingComparatorClass(GroupingComparator1.class);
 
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -39,11 +37,10 @@ public class SalesDriver extends Configured
          throw new IllegalArgumentException
                                    ("usage: <input> <output>");
        }
-
        THE_LOGGER.info("inputDir = " + args[0]);
        THE_LOGGER.info("outputDir = " + args[1]);
        int returnStatus = ToolRunner.
-                     run(new SalesDriver(), args);
+                     run(new Driver(), args);
        THE_LOGGER.info("returnStatus=" + returnStatus);
        System.exit(returnStatus);
     }
