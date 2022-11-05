90ee209ae85bc09882894d850e6dae13d3697f59
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-04 20:08:48 -0700

    argh why am I like this

--- lab6/src/main/scala/com/App.scala
+++ lab6/src/main/scala/com/App.scala
@@ -7,41 +7,47 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
 
-
 object App {
- def main(args: Array[String]): Unit = {
-     Logger.getLogger("org").setLevel(Level.OFF)
-     Logger.getLogger("akka").setLevel(Level.OFF)
+    def main(args: Array[String]): Unit = {
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
 
     val conf = new SparkConf().setAppName("NameOfApp")
-                                    // .setMaster("local[4]")
     val sc = new SparkContext(conf)
-    //parameter to setMaster tells us how to distribute
-    // the data, e.g., 4 partitions on the localhost
-    //don't use setMaster if running on cluster
 
-    /* productID, price */
     val products = product(sc)
+    val lineItems = lineItem(sc)
+    val stores = store(sc)
+    val sales = sale(sc)
 
-    val lineItems = sc.textFile("lineItem.csv")
-      .map(x => (x.split(", ")(1), x.split(", ")))
-      .groupByKey()
-      .mapValues(x => x.foldRight(0.0)((y,z) => products(y._2) * y(3).toInt + z))
-      .foreach(println(_))
+    // salesID, productID, price*quantity then join by salesID
+    // .join sales, .join stores -> storeID, state, salesID, money then groupby storeID, sort by state, and print
 
-    // val sales = sc.textFile("lineItem.csv").map(x =>
-    //     (x.split(",")(0), x.split(", ")(1), x.split(", ")(2), x.split(", ")(3).toDouble))
-    //     .map(x => (x._2, (products(x._3) * x._4)))
-    // val saleTotal = sales.groupBy(x => (x._1, x._2)).mapValues(_.map(_._2).sum)
-    // val storeTotal = sc.textFile("sales.csv").map(x =>
-    //     (x.split(", ")(0), x.split(", ")(1), x.split(", ")(2), x.split(", ")(3), x.split(", ")(4)))
-    //     .map(x => (x._4, saleTotal(x._1))).groupBy(x => (x._1, x._2)).mapValues(_.map(_._2).sum)
-    // sc.textFile("store.csv").map(x =>
-    //     (x.split(", ")(0), x.split(", ")(1), x.split(", ")(2), x.split(", ")(3), x.split(", ")(4),x.split(", ")(5),x.split(", ")(6)))
-    //     .map(x =>(x._6, x._1, saleTotal(x._1))).sortBy(x => (x._1, x._2)).foreach(println(_))
-  }
+    // state, storeID, sales
 
-  def product(sc : SparkContext): RDD[(String, Double)] = {
-    return sc.textFile("product.csv").map(x => (x.split(", ")(0), x.split(", ")(2).toDouble))
-  }
+    }
+
+    // productID, description, price -> (productID, price)
+    def product(sc : SparkContext): RDD[(String, Double)] = {
+        return sc.textFile("product.csv").map(x =>
+        (x.split(", ")(0), x.split(", ")(2).toDouble))
+    }
+
+    // lineItemID, salesID, productID, quantity -> (salesID, productID, quantity)
+    def lineItem(sc : SparkContext): RDD[(String, String, Double)] = {
+        return sc.textFile("lineItem.csv").map(x =>
+        (x.split(", ")(1), x.split(", ")(2), x.split(", ")(2)))
+    }
+
+    // storeID, storeName, address, city, ZIP, state, phoneNumber -> (storeID, state)
+    def store(sc: SparkContext): RDD[(String, String)] = {
+        return sc.textFile("store.csv").map(x =>
+        (x.split(", ")(0), x.split(", ")(5)))
+    }
+
+    // saleID, date, time, storeID, customerID -> (saleID, storeID)
+    def sale(sc: SparkContext): RDD[(String, String)] = {
+        sc.textFile("sales.csv").map(x =>
+        (x.split(", ")(0), x.split(", ")(3)))
+    }
 }
