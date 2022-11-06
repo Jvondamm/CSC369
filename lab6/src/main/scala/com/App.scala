2dff7ab54dcb5be4c7ca6fdc40b68370681942d6
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-05 17:04:46 -0700

    man wtf

--- lab6/src/main/scala/com/App.scala
+++ lab6/src/main/scala/com/App.scala
@@ -39,7 +39,7 @@ object App {
     // lineItemID, salesID, productID, quantity -> (productID, salesID, quantity)
     def lineItem(sc : SparkContext): RDD[(String, String, Double)] = {
         return sc.textFile("input/lineItem.csv").map(x =>
-        (x.split(",")(2), x.split(",")(1), x.split(",")(3).toDouble))
+        (x.split(",")(2), (x.split(",")(1), x.split(",")(3).toDouble)))
     }
 
     // storeID, storeName, address, city, ZIP, state, phoneNumber -> (storeID, state)
