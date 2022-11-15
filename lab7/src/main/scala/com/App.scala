28cb3644704d1d5e3acc670806ab656027543718
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-15 14:52:24 -0800

    do i dare to hope

--- lab7/src/main/scala/com/App.scala
+++ lab7/src/main/scala/com/App.scala
@@ -29,11 +29,9 @@ object App {
     //.join(stores).sortBy(_._2._2).collect().foreach(println(_))
 
     val job2 = sales.join(stores).mapValues(x => Array(x._1(0), x._1(1).substring(0,7), x._1(2), x._1(3), x._1(4), x._2._1, x._2._2))
-    job2.collect.foreach(println(_))
-
-    // job2.join(job1).map(x => (x._2._1(1), (x._2._1(5), x._2._1(6), x._2._2))).groupByKey().sortByKey()
-    //   .mapValues(x => x.groupBy(_._1).mapValues(x => x.toList.map(x => (x._3))).mapValues(_.sum))
-    //   .map(x => (x._1, x._2.toList.sorted)).collect().foreach(println(_))
+    job2.join(job1).map(x => (x._2._1(1), (x._2._1(5), x._2._1(6), x._2._2))).groupByKey().sortByKey()
+      .mapValues(x => x.groupBy(_._1).mapValues(x => x.toList.map(x => (x._3))).mapValues(_.sum))
+      .map(x => (x._1, x._2.toList.sorted)).collect().foreach(println(_))
     }
 
     // productID, description, price -> (productID, price)
