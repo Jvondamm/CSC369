b3a50ed8e8c74eacc2758aec6c53e11cd9e8a671
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-15 12:26:53 -0800

    wait im back here again fuck me man

--- lab6/src/main/scala/com/App.scala
+++ lab6/src/main/scala/com/App.scala
@@ -21,31 +21,31 @@ object App {
     val stores = store(sc)
     val sales = sale(sc)
 
-    val job1 = lineItems.groupByKey().mapValues(x => x.foldRight(0.0)((y,z) => products(y(2)) * y(3).toInt + z)) 
+    val job1 = lineItems.groupByKey().mapValues(x => x.foldRight(0.0)((y,z) => products(y(2)) * y(3).toInt + z))
     sales.join(job1).map(x => (x._2._1, x._2._2)).groupByKey().mapValues(x => x.sum).join(stores).sortBy(_._2._2).foreach(println(_))
     }
 
     // productID, description, price -> (productID, price)
     def product: Map[String, Double] = {
-        return Source.fromFile("product.csv").getLines().toList.map(x =>
+        return Source.fromFile("/user/jvondamm/input/product.csv").getLines().toList.map(x =>
         (x.split(",")(0),  x.split(",")(2).toDouble)).toMap
     }
 
     // lineItemID, salesID, productID, quantity -> (productID, salesID, quantity)
     def lineItem(sc : SparkContext): RDD[(String, Array[String])] = {
-        return sc.textFile("lineItem.csv").map(x =>
+        return sc.textFile("user/jvondamm/input/lineItem.csv").map(x =>
         (x.split(",")(1), x.split(",")))
     }
 
     // storeID, storeName, address, city, ZIP, state, phoneNumber -> (storeID, state)
     def store(sc: SparkContext): RDD[(String, String)] = {
-        return sc.textFile("store.csv").map(x =>
+        return sc.textFile("user/jvondamm/input/store.csv").map(x =>
         (x.split(",")(0), x.split(",")(5)))
     }
 
     // saleID, date, time, storeID, customerID -> (saleID, storeID)
     def sale(sc: SparkContext): RDD[(String, String)] = {
-        sc.textFile("sales.csv").map(x =>
+        sc.textFile("user/jvondamm/input/sales.csv").map(x =>
         (x.split(",")(0), x.split(",")(3)))
     }
 }
