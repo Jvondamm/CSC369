3490c058f13e15fbe131b0dc1cf8a1c425ccb519
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-15 14:38:46 -0800

    spending too mch time on this fr

--- lab7/src/main/scala/com/App.scala
+++ lab7/src/main/scala/com/App.scala
@@ -23,14 +23,18 @@ object App {
 
     // group by product ID, multiply quantity by price for each product to get sum -> (saleID, productID, sum)
     val job1 = lineItems.groupByKey().mapValues(x => x.foldRight(0.0)((y,z) => products(y(2)) * y(3).toInt + z))
-    // (storeID, (saleID, date, time, storeID, customerID) join to (storeID, state), then make array with:
-    // saleID, date (truncated), time, storeID, customerID, storeID, state
-    val job2 = sales.join(stores).mapValues(x => Array(x._1(0), x._1(1).substring(0,7), x._1(2), x._1(3), x._1(4), x._2._1, x._2._2))
-    job2.collect.foreach(println(_))
 
-    job2.join(job1).map(x => (x._2._1(1), (x._2._1(5), x._2._1(6), x._2._2))).groupByKey().sortByKey()
-      .mapValues(x => x.groupBy(_._1).mapValues(x => x.toList.map(x => (x._3))).mapValues(_.sum))
-      .map(x => (x._1, x._2.toList.sorted)).collect().foreach(println(_))
+    //
+    val job3 = sales.join(job1).map(x => (x._1._1, x._2._1, x._2._2)).groupByKey().mapValues(x => x.sum).collect.foreach(println(_))
+
+    //.join(stores).sortBy(_._2._2).collect().foreach(println(_))
+
+    // val job2 = sales.join(stores).mapValues(x => Array(x._1(0), x._1(1).substring(0,7), x._1(2), x._1(3), x._1(4), x._2._1, x._2._2))
+    // job2.collect.foreach(println(_))
+
+    // job2.join(job1).map(x => (x._2._1(1), (x._2._1(5), x._2._1(6), x._2._2))).groupByKey().sortByKey()
+    //   .mapValues(x => x.groupBy(_._1).mapValues(x => x.toList.map(x => (x._3))).mapValues(_.sum))
+    //   .map(x => (x._1, x._2.toList.sorted)).collect().foreach(println(_))
     }
 
     // productID, description, price -> (productID, price)
@@ -51,9 +55,9 @@ object App {
         (x.split(",")(0), x.split(",")(5)))
     }
 
-    // saleID, date, time, storeID, customerID -> (storeID, (saleID, date, time, storeID, customerID))
-    def sale(sc: SparkContext): RDD[(String, String)] = {
+    // saleID, date, time, storeID, customerID -> (saleID, month, storeID)
+    def sale(sc: SparkContext): RDD[(String, String, String)] = {
         sc.textFile("/user/jvondamm/input/sales.csv").map(x =>
-        (x.split(",")(3), x.split(",")))
+        (x.split(",")(0), x.split(",")(2).substring(0,7), x.split(",")(3)))
     }
 }
