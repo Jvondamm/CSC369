0b525aa0b2c17752f0362b196d826a6b038096eb
joshuavd <joshua.vondamm@packetcraft.com>
2022-11-15 14:44:59 -0800

    am i making progress? probably not

--- lab7/src/main/scala/com/App.scala
+++ lab7/src/main/scala/com/App.scala
@@ -24,13 +24,12 @@ object App {
     // group by product ID, multiply quantity by price for each product to get sum -> (saleID, productID, sum)
     val job1 = lineItems.groupByKey().mapValues(x => x.foldRight(0.0)((y,z) => products(y(2)) * y(3).toInt + z))
 
-    //
-    val job3 = sales.join(job1).map(x => (x._1._1, x._2._1, x._2._2)).groupByKey().mapValues(x => x.sum).collect.foreach(println(_))
+    //val job3 = sales.join(job1).map(x => (x._1._1, x._2._1, x._2._2)).groupByKey().mapValues(x => x.sum).collect.foreach(println(_))
 
     //.join(stores).sortBy(_._2._2).collect().foreach(println(_))
 
-    // val job2 = sales.join(stores).mapValues(x => Array(x._1(0), x._1(1).substring(0,7), x._1(2), x._1(3), x._1(4), x._2._1, x._2._2))
-    // job2.collect.foreach(println(_))
+    val job2 = sales.join(stores).mapValues(x => Array(x._1(0), x._1(1).substring(0,7), x._1(2), x._1(3), x._1(4), x._2._1, x._2._2))
+    job2.collect.foreach(println(_))
 
     // job2.join(job1).map(x => (x._2._1(1), (x._2._1(5), x._2._1(6), x._2._2))).groupByKey().sortByKey()
     //   .mapValues(x => x.groupBy(_._1).mapValues(x => x.toList.map(x => (x._3))).mapValues(_.sum))
@@ -49,15 +48,15 @@ object App {
         (x.split(",")(1), x.split(",")))
     }
 
-    // storeID, storeName, address, city, ZIP, state, phoneNumber -> (storeID, state)
-    def store(sc: SparkContext): RDD[(String, String)] = {
+    // storeID, storeName, address, city, ZIP, state, phoneNumber -> (storeID, storeName, city)
+    def store(sc: SparkContext): RDD[(String, String, String)] = {
         return sc.textFile("/user/jvondamm/input/store.csv").map(x =>
-        (x.split(",")(0), x.split(",")(5)))
+        (x.split(",")(0), x.split(",")(1), x.split(",")(3)))
     }
 
     // saleID, date, time, storeID, customerID -> (saleID, month, storeID)
-    def sale(sc: SparkContext): RDD[(String, String, String)] = {
+    def sale(sc: SparkContext): RDD[(String, Array[String])] = {
         sc.textFile("/user/jvondamm/input/sales.csv").map(x =>
-        (x.split(",")(0), x.split(",")(2).substring(0,7), x.split(",")(3)))
+        (x.split(",")(0), x.split(",")))
     }
 }
